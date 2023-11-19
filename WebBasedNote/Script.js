f5146089b360f9b799fc513d2c8b55b183c56466
Kaustubh <110345555+KaustubhWahane@users.noreply.github.com>
2023-11-19 16:26:05 +0530

    Delete WebBasedNote directory

--- WebBasedNote/Script.js
+++
@@ -1,9 +0,0 @@
-function clickKiya(){
-    let naam = document.getElementById('name');
-    let Umra = document.getElementById('age');
-    let StudentId = document.getElementById('sid');
-    let Ghar = document.getElementById('address');
-    
-    let details = document.getElementById('details')
-    console.log(details)
-}
